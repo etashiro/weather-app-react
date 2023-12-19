3774792a2c8da473c01768ea430f4ada4e375e90
etashiro <142271606+etashiro@users.noreply.github.com>
2023-12-18 17:06:26 -0800

    Added weather forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,45 @@
+import React, { useState, useEffect } from "react";
+import Axios from "axios";
+import "./WeatherForecast.css";
+import WeatherForecastDay from "./WeatherForecastDay.js";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.searchedCity]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="WeatherForecast">
+        <div className="row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    const apiKey = "34386e004af6ea0365btbb6ff72e0aoe";
+    let units = "imperial";
+    let city = props.searchedCity;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=${units}`;
+
+    Axios.get(apiUrl).then(handleResponse);
+
+    return null;
+  }
+}
